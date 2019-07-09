c1a81fd30621ba9d8401c41b3eec29d3d168bf09
Lucas Habrich <lucas.habrich@gmx.de>
2019-07-09 20:35:43 +0200

    Added basic pages management to admin panel

--- lib/degree_web/controllers/admin/admin_controller.ex
+++ lib/degree_web/controllers/admin/admin_controller.ex
@@ -1,7 +1,22 @@
 defmodule DegreeWeb.AdminController do
   use DegreeWeb, :controller
+  
+  alias Degree.Repo
 
   def index(conn, _params) do
     render(conn, "index.html")
+  end
+
+  def index_pages(conn, _params) do
+  	render(conn, "index_pages.html", routes: Thesis.Page |> Repo.all)
+  end
+
+  def delete_page(conn, %{"route_id" => route_id}) do
+  	page = Repo.get(Thesis.Page, route_id)
+    Thesis.EctoStore.delete(%{"slug" => page.slug})
+
+    conn
+    |> put_flash(:info, "Page deleted successfully.")
+    |> redirect(to: Routes.admin_path(conn, :index_pages))
   end
 end
