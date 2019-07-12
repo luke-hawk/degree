b96438fef3539adcdd7cfa5e337c8e07221c6e74
luke-hawk <25054@novasbe.pt>
2019-07-12 15:09:55 +0200

    Fixed Thesis authentication

--- lib/degree/thesis_auth.ex
+++ lib/degree/thesis_auth.ex
@@ -2,11 +2,24 @@ defmodule Degree.ThesisAuth do
   @moduledoc """
   Contains functions for handling Thesis authorization.
   """
+  import Plug.Conn
+  import Logger
+
   @behaviour Thesis.Auth
 
   def page_is_editable?(conn) do
-    IO.inspect(Guardian.Plug.current_resource(conn))
+    
+    case verify_session(conn) do
+      {:ok, success} ->
+        true
+      {:error, error} ->
+        false
+    end
+  end
 
-  	true
+  defp verify_session(conn) do
+    conn
+    |> get_session(:guardian_admin_token)
+    |> Degree.Auth.Guardian.decode_and_verify
   end
 end
